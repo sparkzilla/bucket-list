a10f6186913bbb38c664f100aa047a2b63e498a0
user <user@users-MacBook-Pro-9.local>
2019-11-30 13:41:31 +0000

    Add User.save method. Update seeds, tested.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 require('pry')
-require_relative('models/user')
-require_relative('models/sight')
-require_relative('models/city')
-require_relative('models/country')
+require_relative('../models/users')
+require_relative('../models/sights')
+# require_relative('models/city')
+# require_relative('models/country')
 
 #User.delete_all()
-user1 = Customer.new(
+user1 = User.new(
   {
     'first_name' => "Bob",
     'last_name' => "Andrews"
@@ -17,17 +17,17 @@ sight1 = Sight.new({
   'name' => 'Tokyo Tower',
   'user_id' => user1.id
 })
-sight1.save()
+#sight1.save()
 
-city1 = City.new({
-  'name' => 'Tokyo',
-  'sight_id' => site1.id
-  })
-
-country1 = Country.new({
-  'name' => 'Japan',
-  'sight_id' => city1.id
-  })
+# city1 = City.new({
+#   'name' => 'Tokyo',
+#   'sight_id' => site1.id
+#   })
+#
+# country1 = Country.new({
+#   'name' => 'Japan',
+#   'sight_id' => city1.id
+#   })
 
 
 
--- models/users.rb
+++ models/users.rb
@@ -11,4 +11,11 @@ class User
       @last_name = options['last_name']
   end
 
+  def save()
+    sql = "INSERT INTO users (first_name, last_name) VALUES ($1, $2) RETURNING id"
+    values = [@first_name, @last_name]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id'].to_i()
+  end
+
 end
