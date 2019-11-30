5e140e496a368a95fb2cee5a87323b6f05e1d49d
user <user@users-MacBook-Pro-9.local>
2019-11-30 18:53:36 +0000

    Add Sights.all

--- controllers/sights.rb
+++ controllers/sights.rb
@@ -1,17 +1,8 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('../db/seeds')
-require_relative( '../models/users' )
+require_relative( '../models/sights' )
 also_reload( '../models/*' )
-
-
-
-get '/sights' do
-  @users = User.all()
-  erb ( :"sights/index" )
-end
-
-
 
 get '/sights' do
   @sights = Sight.all()
