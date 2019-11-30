2369e27ba393eedc626550d256f8baeb67e8d268
user <user@users-MacBook-Pro-9.local>
2019-11-30 19:55:52 +0000

    Tested Sight methods. All good.

--- models/sights.rb
+++ models/sights.rb
@@ -2,17 +2,18 @@ require_relative('../db/sql_runner')
 
 class Sight
 
-  attr_accessor :name
+  attr_accessor :name, :status
   attr_reader :id
 
   def initialize(options)
       @id = options['id'].to_i() if options['id']
       @name = options['name']
+      @status = options['status']
   end
 
   def save()
-    sql = "INSERT INTO sights (name) VALUES ($1) RETURNING id"
-    values = [@name]
+    sql = "INSERT INTO sights (name, status) VALUES ($1, $2) RETURNING id"
+    values = [@name, @status]
     result = SqlRunner.run(sql, values)
     @id = result[0]['id'].to_i()
   end
