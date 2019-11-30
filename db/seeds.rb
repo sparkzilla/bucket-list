df41060187136fb57f8bab18814abe752bbdc9df
user <user@users-MacBook-Pro-9.local>
2019-11-30 16:26:55 +0000

    Added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,13 +21,37 @@ sight1 = Sight.new({
 })
 sight1.save()
 
+sight2 = Sight.new({
+  'name' => 'Eifell Tower',
+})
+sight2.save()
+
+sight3 = Sight.new({
+  'name' => 'The Pyramids',
+})
+sight3.save()
+
 visit1 = Visit.new({
   'user_id' => user1.id,
   'sight_id' => sight1.id,
   'status' => "visited"
   })
-
 visit1.save()
+
+visit2 = Visit.new({
+  'user_id' => user1.id,
+  'sight_id' => sight2.id,
+  'status' => "bucket"
+  })
+visit2.save()
+
+visit3 = Visit.new({
+  'user_id' => user1.id,
+  'sight_id' => sight3.id,
+  'status' => "bucket"
+  })
+visit3.save()
+
 
 
 binding.pry
