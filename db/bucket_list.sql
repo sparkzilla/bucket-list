6335344210b0bb020917996eeb363c3ebcf19719
user <user@users-MacBook-Pro-9.local>
2019-11-30 19:42:57 +0000

    New sights table

--- db/bucket_list.sql
+++ db/bucket_list.sql
@@ -1,21 +1,7 @@
-DROP TABLE visits;
 DROP TABLE sights;
-DROP TABLE users;
-
-CREATE TABLE users (
-  id SERIAL8 PRIMARY KEY,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255)
-);
 
 CREATE TABLE sights (
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255)
-);
-
-CREATE TABLE visits (
-  id SERIAL8 PRIMARY KEY,
-  user_id INT REFERENCES users(id) ON DELETE CASCADE,
-  sight_id INT REFERENCES sights(id) ON DELETE CASCADE,
-  status VARCHAR
+  status VARCHAR(255)
 );
