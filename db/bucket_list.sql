18c9503aadf3bace8b743460dd698f884a22ea89
user <user@users-MacBook-Pro-9.local>
2019-11-30 22:43:46 +0000

    add city to db

--- db/bucket_list.sql
+++ db/bucket_list.sql
@@ -1,7 +1,14 @@
 DROP TABLE sights;
+DROP TABLE cities;
+
+CREATE TABLE cities (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+)
 
 CREATE TABLE sights (
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255),
   status VARCHAR(255)
+  city_id INT REFERENCES cities(id)
 );
