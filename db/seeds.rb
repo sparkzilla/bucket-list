dffcffe84f5a1ec2b778ef229d51f45b8a890c7d
user <user@users-MacBook-Pro-9.local>
2019-11-30 19:42:27 +0000

    Write seeds file only for sights

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,63 +1,40 @@
 require('pry')
-require_relative('../models/users')
 require_relative('../models/sights')
-require_relative('../models/visits')
 # require_relative('models/city')
 # require_relative('models/country')
 
 Sight.delete_all()
-User.delete_all()
-Visit.delete_all()
-
-user1 = User.new({
-    'first_name' => "Bob",
-    'last_name' => "Andrews"
-  })
-
-user1.save()
 
 sight1 = Sight.new({
   'name' => 'Tokyo Tower',
+  'status' => 'visited'
 })
 sight1.save()
 
 sight2 = Sight.new({
   'name' => 'Eifell Tower',
+  'status' => 'visited'
 })
 sight2.save()
 
 sight3 = Sight.new({
   'name' => 'The Pyramids',
+  'status' => 'bucket'
 })
 sight3.save()
 
 sight4 = Sight.new({
   'name' => 'Empire State Building',
+  'status' => 'visited'
 })
 sight4.save()
 
-visit1 = Visit.new({
-  'user_id' => user1.id,
-  'sight_id' => sight1.id,
-  'status' => "visited"
-  })
-visit1.save()
-
-visit2 = Visit.new({
-  'user_id' => user1.id,
-  'sight_id' => sight2.id,
-  'status' => "bucket"
-  })
-visit2.save()
-
-visit3 = Visit.new({
-  'user_id' => user1.id,
-  'sight_id' => sight3.id,
-  'status' => "bucket"
-  })
-visit3.save()
+sight5 = Sight.new({
+  'name' => '',
+  'status' => 'bucket'
+})
+sight5.save()
 
 
-
-# binding.pry
-# nil
+binding.pry
+nil
