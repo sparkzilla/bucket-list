1145cf0c9fb77f556306f090fa5c042a6b628265
user <user@users-MacBook-Pro-9.local>
2019-12-01 17:03:15 +0000

    Add city and sight from a single form

--- controllers/sights.rb
+++ controllers/sights.rb
@@ -12,6 +12,7 @@ end
 
 get '/sights/new' do
   @sights = Sight.all
+  @cities = City.all
   erb (:"/sights/new")
 end
 
@@ -25,11 +26,39 @@ get '/sights/status/:status' do
   erb ( :"sights/index" )
 end
 
+##{params[:int1].to_i + params[:int2].to_i}"
+
 post '/sights' do
-  sight = Sight.new(params)
+  #get the city param
+  #save city
+  #use city.id in Sight params
+  #extract the other params (name, status) OR remove city
+  #create a new hash with name, status, city_id
+  #save sight
+
+  city_param = params[:city]
+  new_city = {"name" => "#{city_param}"}
+  city = City.new(new_city)
+  city.save
+
+  status_param = params[:status]
+  sight_name_param = params[:name]
+
+  new_sight = {
+    "name" => "#{sight_name_param}",
+    "status" => "#{status_param}",
+    "city_id" => city.id.to_s
+  }
+
+  sight = Sight.new(new_sight)
   sight.save
   redirect to("/sights")
 end
+
+  # sight = Sight.new(params)
+  # city.save
+  # sight.save
+  # redirect to("/sights")
 
 post '/sights/:id/delete' do
   Sight.delete_by_id(params['id'].to_i)
