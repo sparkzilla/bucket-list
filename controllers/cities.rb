4094d2dec789ccac962ecbae04452df625cb59bd
user <user@users-MacBook-Pro-9.local>
2019-12-03 23:30:01 +0000

    Get map working on cities page

--- controllers/cities.rb
+++ controllers/cities.rb
@@ -6,7 +6,6 @@ also_reload( '../models/*' )
 
 get '/cities' do
   @cities = City.all()
-  erb (:"cities/index")
 end
 
 get '/cities/new' do
@@ -16,6 +15,8 @@ end
 
 get '/cities/:id' do
   @city = City.find_by_id(params['id'].to_i)
+  @url = ""
+  @map_url = "https://www.mapquestapi.com/staticmap/v5/map?key=wGML8wgmXkCrOW0ymESQKWNbOUCNQI2D&&size=1390,375&type=light&locations="
   erb(:"cities/show")
 end
 
